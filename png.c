f523544efef0987a09814c346cdc286db2cd3949
Jeff Muizelaar <jmuizelaar@mozilla.com>
2010-10-04 13:52:32 -0400

    Some quick cleanup

--- png.c
+++ png.c
@@ -35,7 +35,6 @@ png_byte bit_depth;
 png_structp png_ptr;
 png_infop info_ptr;
 int number_of_passes;
-//png_bytep * row_pointers;
 
 png_bytep *read_png_file(char* file_name)
 {
@@ -53,7 +52,7 @@ png_bytep *read_png_file(char* file_name)
 
 	/* initialize stuff */
 	png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-	
+
 	if (!png_ptr)
 		abort_("[read_png_file] png_create_read_struct failed");
 
@@ -93,62 +92,6 @@ png_bytep *read_png_file(char* file_name)
 }
 
 
-void write_png_file(char* file_name, png_bytep *row_pointers)
-{
-	/* create file */
-	FILE *fp = fopen(file_name, "wb");
-	if (!fp)
-		abort_("[write_png_file] File %s could not be opened for writing", file_name);
-
-
-	/* initialize stuff */
-	png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-	
-	if (!png_ptr)
-		abort_("[write_png_file] png_create_write_struct failed");
-
-	info_ptr = png_create_info_struct(png_ptr);
-	if (!info_ptr)
-		abort_("[write_png_file] png_create_info_struct failed");
-
-	if (setjmp(png_jmpbuf(png_ptr)))
-		abort_("[write_png_file] Error during init_io");
-
-	png_init_io(png_ptr, fp);
-
-
-	/* write header */
-	if (setjmp(png_jmpbuf(png_ptr)))
-		abort_("[write_png_file] Error during writing header");
-
-	png_set_IHDR(png_ptr, info_ptr, width, height,
-		     bit_depth, color_type, PNG_INTERLACE_NONE,
-		     PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_BASE);
-
-	png_write_info(png_ptr, info_ptr);
-
-
-	/* write bytes */
-	if (setjmp(png_jmpbuf(png_ptr)))
-		abort_("[write_png_file] Error during writing bytes");
-
-	png_write_image(png_ptr, row_pointers);
-
-
-	/* end write */
-	if (setjmp(png_jmpbuf(png_ptr)))
-		abort_("[write_png_file] Error during end of write");
-
-	png_write_end(png_ptr, NULL);
-
-        /* cleanup heap allocation */
-	for (y=0; y<height; y++)
-		free(row_pointers[y]);
-	free(row_pointers);
-
-        fclose(fp);
-}
-
 int clamp(int a)
 {
 	if (a > 255)
@@ -171,9 +114,6 @@ int vc(int r, int g, int b)
 {
 	return clamp(128 + 112.439*r/256 - 94.154*g/256 - 18.285*b/256);
 }
-
-
-
 
 void process_file(png_bytep * row_pointers, png_bytep *row_pointers2)
 {
